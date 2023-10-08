4273dd4f2a29fdcf60236d0376e7c5777b27057a
ssakibs04 <sakibshadman34@gmail.com>
2023-10-08 20:26:41 +0600

    First

---
+++ Lab4/Lab4/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Lab4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+
+
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(SignUp s)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction("Login");
+            }
+            return View(s);
+        }
+
+
+    }
+    }
---
+++ Lab4/Lab4/Models/SignUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab4.Models
+{
+    public class SignUp
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Name must be between 4 and 50 characters.")]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Name must contain only letters.")]
+        public string Name  { get; set; }
+
+
+
+        [Required (ErrorMessage ="UserID is Required")]
+        [StringLength(12 ,MinimumLength =4 ,ErrorMessage ="UserId must be between 4 and 12 characters.")]
+        [RegularExpression("^[a-zA-Z0-9_-]*$", ErrorMessage = "UserID must contain only letters, numbers, underscores, or dashes.")]
+        public string UserID { get; set; }
+
+
+        [Required(ErrorMessage ="PassWord Is Required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")] 
+        public string Password { get; set; }
+     
+        [Required (ErrorMessage="ID is Required")]
+        [RegularExpression(@"^\d{2}-\d{5}-\d{1}$", ErrorMessage = "ID must be in the format **-*****-*.")]
+        public string ID { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [RegularExpression(@"\d{2}-\d{5}-\d{1}$*@student@aiub@edu$", ErrorMessage = "Invalid Email format.")]
+        public string Email { get; set; }
+        [Required]
+
+        public Nullable <System.DateTime> DOB {  get; set; }
+    }
+}
